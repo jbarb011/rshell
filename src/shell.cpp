975768f43b153163e75eac939ca2b84fda6f432a
Jerome Barbero <jbarb011@hammer.cs.ucr.edu>
2015-04-12 23:21:12 -0700

    made extra credit work

--- src/shell.cpp
+++ src/shell.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 #include <unistd.h>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+    bool execute = true;
+
+    while(execute){
+        char* username;
+        char hostname[128];
+        username = getlogin();
+        gethostname(hostname, 128);
+        cerr << username << "@" << hostname << "$ ";
+        execute = false;
+    }
+
     return 0;
 }
