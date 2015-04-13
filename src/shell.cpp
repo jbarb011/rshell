50ed78853469854e190f9502c5573c33adb79b4e
Jerome Barbero <jbarb011@hammer.cs.ucr.edu>
2015-04-12 23:31:26 -0700

    last one for tonight: added some command code

--- src/shell.cpp
+++ src/shell.cpp
@@ -9,12 +9,20 @@ int main()
     bool execute = true;
 
     while(execute){
+
+        //extra credit part
         char* username;
         char hostname[128];
         username = getlogin();
         gethostname(hostname, 128);
         cerr << username << "@" << hostname << "$ ";
-        execute = false;
+        
+        //get command
+        string fullcommand;
+        getline(cin, fullcommand);
+
+
+
     }
 
     return 0;
